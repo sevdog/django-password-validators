b5a705b595764a43b2b16e1750e7c8af129f80d1
Petr Dlouhý <petr.dlouhy@email.cz>
2020-06-11 11:15:08 +0200

    add PasswordCharacterValidator

---
+++ django_password_validators/password_character_requirements/password_validation.py
@@ -0,0 +1,64 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext as _
+
+
+class PasswordCharacterValidator():
+
+    def __init__(
+            self,
+            min_length_digit=1,
+            min_length_alpha=1,
+            min_length_special=1,
+            min_length_lower=1,
+            min_length_upper=1,
+            special_characters="[~!@#$%^&*()_+{}\":;'[]"
+    ):
+        self.min_length_digit = min_length_digit
+        self.min_length_alpha = min_length_alpha
+        self.min_length_special = min_length_special
+        self.min_length_lower = min_length_lower
+        self.min_length_upper = min_length_upper
+        self.special_characters = special_characters
+
+    def validate(self, password, user=None):
+        validation_errors = []
+        if len([char for char in password if char.isdigit()]) < self.min_length_digit:
+            validation_errors.append(ValidationError(
+                _('Password must contain at least %(min_length)d digit.'),
+                params={'min_length': self.min_length_digit},
+                code='min_length_digit',
+            ))
+        if len([char for char in password if char.isalpha()]) < self.min_length_alpha:
+            validation_errors.append(ValidationError(
+                _('Password must contain at least %(min_length)d letter.'),
+                params={'min_length': self.min_length_alpha},
+                code='min_length_alpha',
+            ))
+        if len([char for char in password if char.isupper()]) < self.min_length_upper:
+            validation_errors.append(ValidationError(
+                _('Password must contain at least %(min_length)d capital letter.'),
+                params={'min_length': self.min_length_upper},
+                code='min_length_upper_characters',
+            ))
+        if len([char for char in password if char.islower()]) < self.min_length_lower:
+            validation_errors.append(ValidationError(
+                _('Password must contain at least %(min_length)d small letter.'),
+                params={'min_length': self.min_length_lower},
+                code='min_length_lower_characters',
+            ))
+        if len([char for char in password if char in self.special_characters]) < self.min_length_special:
+            validation_errors.append(ValidationError(
+                _('Password must contain at least %(min_length)d special character.'),
+                params={'min_length': self.min_length_special},
+                code='min_length_special_characters',
+            ))
+        if validation_errors:
+            raise ValidationError(validation_errors)
+
+    def get_help_text(self):
+        return (
+            f"Password must contaion at least "
+            f"{self.min_length_digit} digits, "
+            f"{self.min_length_alpha} letters and "
+            f"{self.min_length_special} special characters"
+        )
